4d53b7689124b6110931c226430a6cd4348050d0
Abraham Panduro <panduroab@gmail.com>
2018-02-15 12:58:49 -0600

    quickfix

--- __tests__/server.js
+++ __tests__/server.js
@@ -2,7 +2,7 @@ const { port } = require('./consts');
 const app = require('../src/server')();
 
 describe('Application', () => {
-    it('should run a app instance', done => {
+    it('should run an app instance', done => {
         app.listen(port, done);
     });
 });
